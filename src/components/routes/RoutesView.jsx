6525f08762d5bc3e2278161c96ae7279b6b62126
Warren Simpson <warren.simpson34@gmail.com>
2020-06-18 11:55:39 -0400

    Changed the route

--- src/components/routes/RoutesView.jsx
+++ src/components/routes/RoutesView.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import {
-  AllCampusesContainer,
-  CampusContainer,
-  AddCampusFormContainer,
-} from "../containers";
+import { WelcomeContainer } from "../containers";
 
 const RoutesView = () => {
   return (
     <Switch>
-      <Route exact path="/" component={AllCampusesContainer} />
-      <Route exact path="/campuses" component={AllCampusesContainer} />
-      <Route exact path="/campuses/new" component={AddCampusFormContainer} />
-      <Route exact path="/campuses/:id" component={CampusContainer} />
+      <Route exact path="/" component={WelcomeContainer} />
     </Switch>
   );
 };
